32d0d813cc10d90cedc2e19bc4d3569c08b1ff00
ssenyaaa <ksenbel73@gmail.com>
2026-02-17 18:54:59 +0400

    Добавьте файлы проекта.

---
+++ лаба 1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using лаба_1;
+
+Book MyBook = new Book()
+{
+    ISBN = "978-5-699-12014-7",
+    NameBook = "Война и мир",
+    Author = "Лев Толстой",
+    Year = 1869,
+    IsAvailable = true,
+    Status = "Доступна"
+
+};
+
+Console.WriteLine($"ISBN книги: {MyBook.ISBN}");
+Console.WriteLine($"Название книги: {MyBook.NameBook}");
+Console.WriteLine($"Автор книги: {MyBook.Author}");
+Console.WriteLine($"Год издания: {MyBook.Year}");
+Console.WriteLine($"Доступность: {MyBook.IsAvailable}");
+Console.WriteLine($"Статус: {MyBook.Status}");
+
+Console.WriteLine("------------------------------------------ ");
+
+MyBook.TakeBook("Анна Смит");
+Console.WriteLine($"Статус после взятия: {MyBook.Status}");
+Console.WriteLine($"Доступность: {MyBook.IsAvailable}");
